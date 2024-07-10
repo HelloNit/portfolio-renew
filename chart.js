d1e12907b81d5f37545ab8cd8bdc78b4648906d0
HelloNit <feliperodrigues0_0@hotmail.com>
2024-07-10 10:30:01 -0300

    Sessão pontos de compreensão e dores adicionadas

--- chart.js
+++ chart.js
@@ -3,7 +3,7 @@ const ctx = document.getElementById('ageChart');
 new Chart(ctx, {
     type: 'doughnut',
     data: {
-    labels: ['17 - 20 Anos', '21 - 30 Anos','31 - 40 Anos', '41 Anos mais'],
+    //labels: ['17 - 20 Anos', '21 - 30 Anos','31 - 40 Anos', '41 Anos mais'],
     datasets: [{
     label: '',
     data: [9, 14, 5, 0],
@@ -13,7 +13,7 @@ new Chart(ctx, {
       'rgb(255, 102, 0)',
       'rgb(204, 51, 153)'
     ],
-    hoverOffset: 4
+    hoverOffset: 6
   }]
   },
 });
@@ -23,7 +23,7 @@ const ctx1 = document.getElementById('workChart');
 new Chart(ctx1, {
     type: 'doughnut',
     data: {
-    labels: ['Sim', 'Não'],
+    //labels: ['Sim', 'Não'],
     datasets: [{
     label: '',
     data: [20, 8],
@@ -31,7 +31,7 @@ new Chart(ctx1, {
       'rgb(7, 244, 158)',
       'rgb(255, 102, 102)'
     ],
-    hoverOffset: 4
+    hoverOffset: 6
   }]
   },
 });
@@ -41,7 +41,7 @@ const ctx2 = document.getElementById('feelingChart');
 new Chart(ctx2, {
     type: 'doughnut',
     data: {
-    labels: ['Sim', 'Não'],
+    //labels: ['Sim', 'Não'],
     datasets: [{
     label: '',
     data: [26, 2],
@@ -49,7 +49,7 @@ new Chart(ctx2, {
       'rgb(7, 244, 158)',
       'rgb(255, 102, 102)'
     ],
-    hoverOffset: 4
+    hoverOffset: 6
   }]
   },
 });
@@ -60,7 +60,7 @@ const ctx3 = document.getElementById('gamingChart');
 new Chart(ctx3, {
     type: 'doughnut',
     data: {
-    labels: ['League Of Legends', 'Dota 2', 'FreeFire', 'Valorant', 'Fornite', 'Clash Of Clans', 'Tibia ou derivados', 'Outros' ],
+    //labels: ['League Of Legends', 'Dota 2', 'FreeFire', 'Valorant', 'Fornite', 'Clash Of Clans', 'Tibia ou derivados', 'Outros' ],
     datasets: [{
     label: '',
     data: [26, 11, 11, 15, 13, 6, 7, 6],
@@ -85,7 +85,7 @@ const ctx4 = document.getElementById('brandChart');
 new Chart(ctx4, {
     type: 'doughnut',
     data: {
-    labels: ['Df Games', 'GGMax', 'PlayerAuctions', 'Melhor dos Games', 'Desapego Games', 'G2G', 'Nenhuma'],
+    //labels: ['Df Games', 'GGMax', 'PlayerAuctions', 'Melhor dos Games', 'Desapego Games', 'G2G', 'Nenhuma'],
     datasets: [{
     label: 'My First Dataset',
     data: [8, 13, 6, 4, 3, 7, 7, 3],
@@ -98,7 +98,8 @@ new Chart(ctx4, {
       'rgb(51, 153, 204)',
       'rgb(204, 238, 102)'
     ],
-    hoverOffset: 4
+    hoverOffset: 6
+    
   }]
   },
 });
@@ -108,7 +109,7 @@ const ctx5 = document.getElementById('sellChart');
 new Chart(ctx5, {
     type: 'doughnut',
     data: {
-    labels: ['16 a 20', '21 a 30'],
+    //labels: ['16 a 20', '21 a 30'],
     datasets: [{
     label: 'My First Dataset',
     data: [300, 100],
@@ -117,8 +118,8 @@ new Chart(ctx5, {
       'rgb(54, 162, 235)',
       'rgb(255, 205, 86)'
     ],
-    hoverOffset: 4
-  }]
+    hoverOffset: 6
+    }]
   },
 });
 
@@ -127,7 +128,7 @@ const ctx6 = document.getElementById('dntChart');
 new Chart(ctx6, {
     type: 'doughnut',
     data: {
-    labels: ['16 a 20', '21 a 30'],
+    //labels: ['16 a 20', '21 a 30'],
     datasets: [{
     label: 'My First Dataset',
     data: [300, 100],
@@ -136,8 +137,8 @@ new Chart(ctx6, {
       'rgb(54, 162, 235)',
       'rgb(255, 205, 86)'
     ],
-    hoverOffset: 4
-  }]
+    hoverOffset: 6
+    }]
   },
 });
 
@@ -146,7 +147,7 @@ const ctx7 = document.getElementById('ecommerceChart');
 new Chart(ctx7, {
     type: 'doughnut',
     data: {
-    labels: ['Amazon', 'Aliexpress', 'Olx', 'Shopee', 'Facebook Marketplace', 'Mercado Livre' ,'Outros'],
+    //labels: ['Amazon', 'Aliexpress', 'Olx', 'Shopee', 'Facebook Marketplace', 'Mercado Livre' ,'Outros'],
     datasets: [{
     label: '',
     data: [23, 18, 16, 13, 6, 12, 3],
@@ -160,8 +161,8 @@ new Chart(ctx7, {
       'rgb(204, 238, 102)',
       'rgb(153, 204, 51)'
     ],
-    hoverOffset: 4
-  }]
+    hoverOffset: 6
+    }]
   },
 });
 
@@ -170,7 +171,7 @@ const ctx8 = document.getElementById('importantChart');
 new Chart(ctx8, {
     type: 'doughnut',
     data: {
-    labels: ['Amazon', 'Aliexpress', 'Olx', 'Shopee', 'Facebook Marketplace', 'Mercado Livre' ,'Outros'],
+    //labels: ['Amazon', 'Aliexpress', 'Olx', 'Shopee', 'Facebook Marketplace', 'Mercado Livre' ,'Outros'],
     datasets: [{
     label: '',
     data: [4, 1, 12, 0, 6, 11, 2],
@@ -184,7 +185,7 @@ new Chart(ctx8, {
       'rgb(204, 238, 102)',
       'rgb(153, 204, 51)'
     ],
-    hoverOffset: 4
-  }]
+    hoverOffset: 6
+    }]
   },
 });
